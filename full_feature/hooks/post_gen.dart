d8a429408547b6395ac57956b57c5bd971506a56
Matias Osuna <osuna.contacto@gmail.com>
2023-07-04 02:21:33 -0300

    removed hook

--- full_feature/hooks/post_gen.dart
+++
@@ -1,7 +0,0 @@
-import 'package:mason/mason.dart';
-import 'dart:io';
-
-Future<void> run(HookContext context) async {
-  await Process.run('melos get', []);
-  await Process.run('melos generate', []);
-}
