3581b6b3fbaeac6ab67f04d9c303c9b5ffbb2487
Matias Osuna <osuna.contacto@gmail.com>
2023-07-04 02:16:09 -0300

    async

--- full_feature/hooks/post_gen.dart
+++ full_feature/hooks/post_gen.dart
@@ -1,6 +1,6 @@
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
+void run(HookContext context) async {
   await Process.run('melos get', []);
   await Process.run('melos generate', []);
 }
