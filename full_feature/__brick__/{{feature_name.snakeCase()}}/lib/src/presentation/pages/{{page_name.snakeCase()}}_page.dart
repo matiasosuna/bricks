9d4652f6426923da4d2a0d167d271abcb2c273f8
Matias Osuna <osuna.contacto@gmail.com>
2023-07-04 01:29:24 -0300

    fix

--- full_feature/__brick__/{{feature_name.snakeCase()}}/lib/src/presentation/pages/{{page_name.snakeCase()}}_page.dart
+++ full_feature/__brick__/{{feature_name.snakeCase()}}/lib/src/presentation/pages/{{page_name.snakeCase()}}_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:{{¬.snakeCase()}}/src/presentation/bloc/{{cubit_name.snakeCase()}}_cubit.dart';
+import 'package:{{feature_name.snakeCase()}}/src/presentation/bloc/{{cubit_name.snakeCase()}}_cubit.dart';
 
 class {{page_name.pascalCase()}}Page extends StatelessWidget {
   const {{page_name.pascalCase()}}Page({super.key, required this.bloc});
